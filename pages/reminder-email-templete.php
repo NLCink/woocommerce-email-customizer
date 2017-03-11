c7b635ca6152b93547c962db37fe881c645a6ace
jakiir <jakir44.du@gmail.com>
2017-03-11 17:49:37 +0600

    reminder email templete

---
+++ pages/reminder-email-templete.php
@@ -0,0 +1,58 @@
+<table class="wrapper" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="font-family: Arial, sans-serif; line-height: 2em; color: #666666; font-size: 14px; background-color: #e8e8e8; width: 100%; -webkit-text-size-adjust: none !important; margin: 0; padding: 50px 0 50px 0;">
+    <tbody>
+      <tr style="font-family: Arial, sans-serif; line-height: 2em;">
+        <td class="wrapper-td" align="center" valign="top" style="font-family: Arial, sans-serif; line-height: 2em; padding: 0 20px 20px;">
+          <table class="main-body" border="0" cellpadding="0" cellspacing="0" style="font-family: Arial, sans-serif; line-height: 2em; color: #666666; box-shadow: 0 3px 9px rgba(0, 0, 0, 0.03); border-radius: 5px !important; overflow: hidden; background-color: #ffffff; border: 1px solid #d1d1d1; width: 800px;">
+            <tbody>
+                <tr style="font-family: Arial, sans-serif; line-height: 2em;">
+                  <td align="center" valign="top" style="font-family: Arial, sans-serif; line-height: 2em;">                
+                  <!-- Header -->
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; line-height: 2em; color: #666666;">
+                      <tbody>
+                        <tr style="font-family: Arial, sans-serif; line-height: 2em;">
+                          <td class="template_header" style="font-family: Arial; line-height: 2em; background-color: #3d3d3d; color: #e2e2e2; border-top-left-radius: 2px !important; border-top-right-radius: 2px !important; border-bottom: 1px solid #3a3a3a; font-weight: bold; vertical-align: middle; text-align: center; padding: 16.666666666667px 23.076923076923px;">
+                            <a href="http://localhost/mehedi/bkacontent_dev" border="0" style="color: #e2e2e2 !important; font-style: none; text-decoration: none; font-weight: normal; font-size: 13px; margin: 0 0 0 12px;">
+                              <img src="http://www.bkacontent.com/wp-content/uploads/2015/10/BKAnewlogowhiteLarge-e1444804499164.png">
+                            </a>                     
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  <!-- End Header -->
+                  </td>
+                </tr>
+                
+                <tr style="font-family: Arial, sans-serif; line-height: 2em;">
+                  <td align="left" valign="top" style="font-family: Arial, sans-serif; line-height: 2em;">                
+                <!-- Body -->
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_body" style="font-family: Arial, sans-serif; line-height: 2em; color: #666666;">
+                  <tbody>
+                  <tr style="font-family: Arial, sans-serif; line-height: 2em;">
+                    <td valign="top" class="body_content" style="font-family: Arial, sans-serif; line-height: 2em; color: #666666; background-color: #ffffff;">         
+                        <!-- Content -->
+                        <table border="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; line-height: 2em; color: #666666;">
+                          <tbody>
+                            <tr style="font-family: Arial, sans-serif; line-height: 2em;">
+                              <td valign="top" class="body_content_inner" style="font-family: Arial; line-height: 2em; text-align: left; padding-left: 55px; padding-right: 55px; padding-top: 30px; padding-bottom: 30px;">
+                                  <p style="margin: .6em 0;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet diam a facilisis eleifend. Cras ac justo felis. Mauris faucibus, orci eu blandit fermentum, lorem nibh sollicitudin mi, sit amet interdum metus urna ut lacus.</p>
+                                  <p style="margin: .6em 0;">Phasellus quis varius augue. Fusce eu euismod leo, a accumsan tellus. Quisque vitae dolor eu justo cursus egestas. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sit amet sapien odio. Sed pellentesque arcu mi, quis malesuada lectus lacinia et. Cras a tempor leo.</p>
+                                  <h2 style="font-family: Arial, sans-serif; color: $body_text_color; font-size: 22px; font-weight: bold; text-align: left;">Lorem ipsum dolor</h2>
+                                  <p style="margin: .6em 0;">Fusce eu euismod leo, a accumsan tellus. Quisque vitae dolor eu justo cursus egestas. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sit amet sapien odio. Sed pellentesque arcu mi, quis malesuada lectus lacinia et. Cras a tempor leo.</p>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <!-- End Content -->
+                      </td>
+                    </tr>
+                    </tbody>
+                </table>
+<!-- End Body -->
+                </td>
+              </tr>
+            </tbody>
+            </table>
+        </td>
+      </tr>
+  </tbody>
+</table>
